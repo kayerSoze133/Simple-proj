6232b4cb9c51d70e7f242e36096ec056f9f49a50
samir <rewritethis100times@yahoo.com>
2020-08-09 14:05:31 +0530

    changed the rotation event listener

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 const outCircle = document.querySelector('.circle.big');
 const inCircle = document.querySelector('.circle.small');
+function isNegative (number) {
+	return (number < 0) ? true : false;
+}
 outCircle.addEventListener('mousemove', e => {
-	let x,y;
-    x,y = [e.layerX,e.layerY];
-    console.log(x,y);
+	if (e.target != e.currentTarget)
+		return;
+	let x,y,angle;
+	[x,y] = [(e.layerX-240),(e.layerY-240)];
+	angle = (Math.atan(Math.abs(y)/Math.abs(x)) * 180) / 3.14;
+	if (isNegative(y)){
+		if (isNegative(x))
+			angle += 180;
+		else
+			angle = 360 - angle;
+	}
+	else{
+		if (isNegative(x))
+			angle = 180 - angle;
+	}
+	console.log(angle);
+	outCircle.style.transform = `translate3d(0,0,0) rotate(${angle}deg)`;
 });
